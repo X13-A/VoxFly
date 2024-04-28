187dea40c8fa98c3111b343647da781cf7969633
lemagnyt <lemagnythomas@gmail.com>
2024-04-28 17:30:11 +0200

    add events

---
+++ Assets/Scripts/AllEvents.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SDD.Events;
+
+#region GameManager Events
+public class GameMenuEvent : SDD.Events.Event
+{
+}
+public class GamePlayEvent : SDD.Events.Event
+{
+}
+public class GamePauseEvent : SDD.Events.Event
+{
+}
+public class GameResumeEvent : SDD.Events.Event
+{
+}
+public class GameOverEvent : SDD.Events.Event
+{
+}
+public class GameVictoryEvent : SDD.Events.Event
+{
+}
+public class GameStatisticsChangedEvent : SDD.Events.Event
+{
+	public int eScore { get; set; }
+	public float eCountDown { get; set; }
+}
+#endregion
+
+#region MenuManager Events
+public class EscapeButtonClickedEvent : SDD.Events.Event
+{
+}
+public class PlayButtonClickedEvent : SDD.Events.Event
+{
+}
+public class ReplayButtonClickedEvent : SDD.Events.Event
+{
+}
+public class ResumeButtonClickedEvent : SDD.Events.Event
+{
+}
+public class MainMenuButtonClickedEvent : SDD.Events.Event
+{
+}
+public class NextLevelButtonClickedEvent : SDD.Events.Event
+{
+}
+#endregion
+
+
+#region Enemy Event
+public class EnemyHasBeenHitEvent : SDD.Events.Event
+{
+	public GameObject eEnemy;
+}
+#endregion
