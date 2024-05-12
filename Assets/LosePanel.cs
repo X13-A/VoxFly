f67d3d8e494fe7f3ae842f32819dc46f8b620f5b
lemagnyt <lemagnythomas@gmail.com>
2024-05-12 23:06:33 +0200

    fix hud and add gameover score

--- Assets/LosePanel.cs
+++ Assets/LosePanel.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LosePanel : MonoBehaviour
 {
     [SerializeField]
     GameObject loseObjects;
+    [SerializeField]
+    TMP_Text scoreText;
+
     void OnEnable()
     {
         StartCoroutine(ActivateAfterDelay());
@@ -19,5 +23,6 @@ public class LosePanel : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         loseObjects.SetActive(true);
+        scoreText.text = string.Format("Final Score : {0:0}", GameManager.Instance.Score);
     }
 }
